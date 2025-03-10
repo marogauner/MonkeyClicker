b6188cc4b24ddfb12cf4eefcc8e55b971c12fb40
marogauner <spektrov2.berraqa@gmail.com>
2025-03-10 11:23:49 +0100

    center + border

--- code/main.js
+++ code/main.js
@@ -2,9 +2,33 @@ function Initialize() {
     var counter = 0;
     document.getElementById("counter").innerHTML = counter;
 }
-function incrementCounter() {
+
+function ClickOnGorilla() {
+    IncrementCounter();
+    PlayAnimation();
+}
+function IncrementCounter() {
     let counter = document.getElementById("counter").innerHTML;
     counter = parseInt(counter);
     counter += 1;
     document.getElementById("counter").innerHTML = counter;
 }
+
+function PlayAnimation() {
+    let gorillaElement = document.getElementsByClassName("transformable")[0]; 
+    if (gorillaElement) {
+        gorillaElement.style.animationPlayState = "running";
+    } else {
+        console.error("Element with class 'transformable' not found.");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let gorillaElement = document.querySelector(".transformable");
+
+    if (gorillaElement) {
+        gorillaElement.addEventListener("animationend", () => {
+            gorillaElement.style.animationPlayState = "paused"; 
+        });
+    }
+});
